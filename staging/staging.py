55ced9e1a5d800e86f6ad02a808ccc4845a1bf29
c-stev <colejstevens@gmail.com>
2024-10-05 18:59:31 -0400

    updated some staging.py functions, added & removed datasets

--- staging/staging.py
+++ staging/staging.py
@@ -1,159 +1,188 @@
 import pandas as pd
+from datasets import load_dataset
 
-# Returns a cleaned dataset sourced from the FA-KES dataset.
-# NOTE: 0 is false, 1 is credible, source: https://zenodo.org/records/2607278
-def get_training_crime():
-    # Reading the dataset and only extracting the useful columns
-    df = pd.read_csv("../data/training/FA-KES.csv", encoding='latin1')[
-        ['article_title', 'article_content', 'labels']
-    ]
-    # Renaming columns
-    df = df.rename(columns={'labels': 'is_true','article_title': 'title', 'article_content': 'text'})
-    # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
+
+# Removes any missing / improper values in the rows
+def stage_df(df, cols):
+    df = df.dropna(subset=cols)
+    for col in cols: df = df[df[col] != ""]
     df = df[df['is_true'].isin([0, 1])]
     return df
 
-# Returns a cleaned dataset sourced from the COVID-FN and COVID-FNIR datasets.
-def get_training_health():
-    df1 = pd.read_csv("../data/COVID19-FNIR/fakeNews.csv")[['Link', 'text', 'Binary Label']] #Covid-FNIR
-    df2 = pd.read_csv("../data/COVID19-FNIR/trueNews.csv")[['Link', 'text', 'Binary Label']] #Covid-FNIR
-    df = pd.concat([df1, df2], ignore_index=True)
-    return df
 
-# Returns a cleaned dataset sourced from the FakeNews, ISOT, and LIAR datasets.
-def get_training_politics():
+def get_combined_isot():
     # Reading the two datasets and only extracting the useful columns
-    df1 = pd.read_csv("../data/ISOT True.csv")[['title', 'text', 'subject']] #ISOT
-    df2 = pd.read_csv("../data/ISOT Fake.csv")[['title', 'text', 'subject']] #ISOT
-    df3 = pd.read_csv("../data/training/FakeNews/train.csv")[['title', 'text', 'label']] #FakeNews
-    # TODO: LIAR dataset
-    df4 = pd.read_json("../data/liar/train/dataset_info.json")[['label', 'statement', 'subject']] #LIAR
-    # Adding an 'is_true' column
-    df1['is_true'] = 1
-    df2['is_true'] = 0
-    # Combining the dataframes (ISOT True and ISOT Fake)
-    df = pd.concat([df1, df2], ignore_index=True)
-    # Renaming columns
-    df = df.rename(columns={'subject': 'category'})
-    df3 = df3.rename(columns={'label': 'is_true'})
-    #df4 = df4.rename(columns={'': ''})
-    # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
-    # Filtering entries to only include Politics articles, and dropping the category column
-    df = df[df['category'].str.contains('politics', case=False, na=False)]
-    df = df.drop(columns=['category'])
-    # Combing the dataframes (ISOT + FakeNews)
-    df = pd.concat([df, df3], ignore_index=True)
-    # TODO: LIAR dataset
-    # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
-    df = df[df['is_true'].isin([0, 1])]
-    return df
-
-# TODO: Climate is a .parquet file, which has had trouble interacting with pandas. Might need a new download?
-# Returns a cleaned dataset sourced from the Climate dataset.
-def get_training_science():
-    df = pd.read_parquet("../data/training/Climate.parquet") #Climate data
-    return df
-
-# TODO: GossipCop dataset only has article titles, no text
-# Returns a cleaned dataset sourced from the GossipCop dataset.
-def get_training_social():
-    # Reading the two datasets and only extracting the useful columns
-    df1 = pd.read_csv("../data/training/GossipCop Real.csv")[['news_url', 'title']]
-    df2 = pd.read_csv("../data/training/GossipCop Fake.csv")[['news_url', 'title']]
+    df1 = pd.read_csv("../data/ISOT True.csv")[['title', 'text', 'subject']]
+    df2 = pd.read_csv("../data/ISOT Fake.csv")[['title', 'text', 'subject']]
     # Adding an 'is_true' column
     df1['is_true'] = 1
     df2['is_true'] = 0
     # Combining the dataframes
     df = pd.concat([df1, df2], ignore_index=True)
-    # TODO: Missing value checks; must wait until we get the text
     return df
 
-# TODO: Snopes dataset only has article titles, no text
+
+# Returns a cleaned dataset sourced from the FA-KES dataset.
+def get_training_crime():
+    # Reading the dataset and only extracting the useful columns
+    df = pd.read_csv("../data/training/FA-KES.csv", encoding='latin1')
+    df = df[['article_title', 'article_content', 'labels']]
+    # Renaming columns
+    df = df.rename(columns={'labels': 'is_true','article_title': 'title', 'article_content': 'text'})
+    # Ensuring there aren't any missing / improper values for the rows
+    df = stage_df(df, df.columns.tolist())
+    return df
+
+
+# Returns a cleaned dataset sourced from the COVID-FN and COVID-FNIR datasets.
+def get_training_health():
+    # Reading the datasets and only extracting the useful columns
+    df1 = pd.read_csv("../data/training/COVID-FNIR-True.csv")[['Text', 'Binary Label']]  # Covid-FNIR
+    df2 = pd.read_csv("../data/training/COVID-FNIR-Fake.csv")[['Text', 'Binary Label']] #Covid-FNIR
+    df3 = pd.read_excel("../data/training/COVID-FN.xlsx")[['text', 'label']]
+    # Renaming columns
+    df1 = df1.rename(columns={'Text': 'text', 'Binary Label': 'is_true'})
+    df2 = df2.rename(columns={'Text': 'text', 'Binary Label': 'is_true'})
+    df3 = df3.rename(columns={'label': 'is_true'})
+    # Combining the dataframes
+    df = pd.concat([df1, df2, df3], ignore_index=True)
+    # Ensuring there aren't any missing / improper values for the rows
+    df = stage_df(df, df.columns.tolist())
+    return df
+
+
+# Returns a cleaned dataset sourced from the FakeNews, ISOT, and LIAR datasets.
+def get_training_politics():
+    # Reading the datasets and only extracting the useful columns
+    df = get_combined_isot() #ISOT
+    df1 = pd.read_csv("../data/training/FakeNews.csv")[['title', 'text', 'label']] #FakeNews
+    df2 = load_dataset("ucsbnlp/liar", split="train").to_pandas()[['statement', 'label']] #LIAR
+    # Renaming columns
+    df1 = df1.rename(columns={'label': 'is_true'})
+    df2 = df2.rename(columns={'statement': 'text', 'label': 'is_true'})
+    # Filtering df entries to only include Politics articles, and dropping the subject column
+    df = df[df['subject'].str.contains('politics', case=False, na=False)]
+    df = df.drop(columns=['subject'])
+    # Fixing df2's is_true column
+    df2['is_true'] = df2['is_true'].replace({0: 0, 1: 0, 2: 1, 3: 1, 4: 0, 5: 0})
+    # Combining the dataframes (ISOT + FakeNews)
+    df = pd.concat([df, df1, df2], ignore_index=True)
+    # Ensuring there aren't any missing / improper values for the rows
+    df = stage_df(df, df.columns.tolist())
+    return df
+
+
+# Returns a cleaned dataset sourced from the Climate dataset.
+def get_training_science():
+    # Reading the dataset and only extracting the useful columns
+    df = pd.read_parquet("hf://datasets/tdiggelm/climate_fever/data/test-00000-of-00001.parquet")
+    df = df[['claim', 'claim_label']]
+    # Renaming columns
+    df = df.rename(columns={'claim_label': 'is_true', 'claim': 'text'})
+    # Fixing the is_true column
+    df['is_true'] = df['is_true'].replace({0: 1, 1: 0, 2: 0, 3: 0})
+    # Ensuring there aren't any missing / improper values for the rows
+    df = stage_df(df, df.columns.tolist())
+    return df
+
+
+# Returns a cleaned dataset sourced from the GossipCop dataset.
+def get_training_social():
+    # Reading the two datasets and only extracting the useful columns
+    df1 = pd.read_csv("../data/training/GossipCop_True.csv")[['title']]
+    df2 = pd.read_csv("../data/training/GossipCop_Fake.csv")[['title']]
+    # Adding an 'is_true' column
+    df1['is_true'] = 1
+    df2['is_true'] = 0
+    # Combining the dataframes
+    df = pd.concat([df1, df2], ignore_index=True)
+    # Ensuring there aren't any missing / improper values for the rows
+    df = stage_df(df, df.columns.tolist())
+    return df
+
+
 # Returns a cleaned dataset sourced from the Snopes dataset.
 def get_testing_crime():
     # Reading the dataset and only extracting the useful columns
-    df = pd.read_csv("../data/training/snopes_phase1_clean_2018_7_3.csv")[
-        ['fact_rating_phase1', 'article_origin_url_phase1', 'article_claim_phase1', 'article_category_phase1']
-    ]
+    df = pd.read_csv("../data/training/Snopes.csv")
+    df = df[['fact_rating_phase1', 'article_claim_phase1', 'article_category_phase1']]
     # Renaming columns
-    df = df.rename(columns={
-        'fact_rating_phase1': 'is_true', 'article_origin_url_phase1': 'source',
-        'article_claim_phase1': 'title', 'article_category_phase1': 'category'
-                            }
-    )
+    df = df.rename(columns={'fact_rating_phase1': 'is_true', 'article_claim_phase1': 'text',
+                            'article_category_phase1': 'category'})
     # Filtering entries to only include Crime articles, and dropping the category column
     df = df[df['category'].str.contains('crime', case=False, na=False)]
     df = df.drop(columns=['category'])
-    # Since snopes doesn't use binary ratings for an article's veracity, we must replace them
-    # Category names were done manually by going through the original Excel file
+    # Fixing the is_true column
     true_values = ["correct attribution", "mostly true", "TRUE"]
     false_values = ["legend", "misattributed", "miscaptioned", "mixture", "mostly false", "outdated", "scam",
                     "unproven", "FALSE"]
     df['is_true'] = df['is_true'].replace(true_values, 1)
     df['is_true'] = df['is_true'].replace(false_values, 0)
     # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'source', 'category'])
-    df = df[(df['title'] != "") & (df['source'] != "") & (df['is_true'] != "")]
-    df = df[df['is_true'].isin([0, 1])]
-    # TODO: Missing value checks; must wait until we get the text
+    df = stage_df(df, df.columns.tolist())
     return df
+
 
 # Returns a cleaned dataset sourced from the COVID_claims dataset.
 def get_testing_health():
     # Reading the two datasets and only extracting the useful columns
-    df1 = pd.read_csv("../data/testing/COVID Claims True.csv")[['Text', 'Label']]
-    df2 = pd.read_csv("../data/testing/COVID Claims False.csv")[['Text', 'Binary Label']]
+    df1 = pd.read_csv("../data/testing/COVID_Claims_True.csv")[['Text', 'Label']]
+    df2 = pd.read_csv("../data/testing/COVID_Claims_Fake.csv")[['Text', 'Binary Label']]
     # Renaming df1 and df2's columns
     df1 = df1.rename(columns={'Text': 'text', 'Label': 'is_true'})
     df2 = df2.rename(columns={'Text': 'text', 'Binary Label': 'is_true'})
     # Combining the dataframes
     df = pd.concat([df1, df2], ignore_index=True)
     # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
+    df = stage_df(df, df.columns.tolist())
     return df
+
 
 # Returns a cleaned dataset sourced from the PHEME and PolitiFact datasets.
 def get_testing_politics():
     df = pd.DataFrame()
     return df
 
-# TODO: ISOT 'subject' doesn't have a science section
+
 # Returns a cleaned dataset sourced from the ISOT dataset.
 def get_testing_science():
-    # Reading the two datasets and only extracting the useful columns
-    df1 = pd.read_csv("../data/ISOT True.csv")[['title', 'text', 'subject']]
-    df2 = pd.read_csv("../data/ISOT Fake.csv")[['title', 'text', 'subject']]
-    # Adding an 'is_true' column
-    df1['is_true'] = 1
-    df2['is_true'] = 0
-    # Combining the dataframes
-    df = pd.concat([df1, df2], ignore_index=True)
+    df = get_combined_isot()
+    # Filtering rows by science keywords (source: https://www.enchantedlearning.com/wordlist/science.shtml)
+    science_terms = [
+        "astronomy", "astrophysics", "atom", "beaker", "biochemistry", "biology", "botany", "Bunsen burner",
+        "burette", "chemical", "chemistry", "climate", "climatologist", "control", "cuvette", "data", "datum",
+        "electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment", "flask",
+        "fossil", "funnel", "genetics", "geology", "geophysics", "glassware", "graduated cylinder", "gravity",
+        "herpetology", "hypothesis", "ichthyology", "immunology", "lab", "laboratory", "lepidoptery",
+        "magnetism", "mass", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology",
+        "microscope", "mineral", "mineralogy", "molecule", "observatory", "observe", "organism", "ornithology",
+        "paleontology", "particle", "Petri dish", "physical science", "physics", "pipette", "quantum mechanics",
+        "radiology", "research", "retort", "science", "scientific method", "scientist", "seismology", "telescope",
+        "temperature", "test tube", "thermometer", "variable", "virologist", "volcanology", "volume",
+        "volumetric flask", "watch glass", "weather", "zoology"
+    ]
+    df = df[df['title'].str.contains('|'.join(science_terms), case=False, na=False) |
+            df['text'].str.contains('|'.join(science_terms), case=False, na=False)]
     # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
-    # TODO: Sort by Science News
+    df = stage_df(df, df.columns.tolist())
     return df
 
-# TODO: ISOT 'subject' doesn't have a social section
+
 # Returns a cleaned dataset sourced from the ISOT dataset.
 def get_testing_social():
-    # Reading the two datasets and only extracting the useful columns
-    df1 = pd.read_csv("../data/ISOT True.csv")[['title', 'text', 'subject']]
-    df2 = pd.read_csv("../data/ISOT Fake.csv")[['title', 'text', 'subject']]
-    # Adding an 'is_true' column
-    df1['is_true'] = 1
-    df2['is_true'] = 0
-    # Combining the dataframes
-    df = pd.concat([df1, df2], ignore_index=True)
+    df = get_combined_isot()
+    # Filtering rows by social keywords (source: ChatGPT)
+    social_terms = [
+        "social media", "Facebook", "Twitter", "Instagram", "LinkedIn", "Snapchat", "YouTube", "TikTok",
+        "Pinterest", "Reddit", "hashtag", "influencer", "vlog", "blog", "feed", "timeline", "profile", "post",
+        "share", "like", "comment", "reaction", "story", "DM", "direct message", "engagement", "followers",
+        "trending", "viral", "content", "analytics", "reach", "impressions", "notification",
+        "user-generated content", "platform", "community", "network", "branding", "advertising", "campaign",
+        "live stream", "memes", "photography", "video", "GIF", "emoticons", "polls", "community guidelines",
+        "privacy", "terms of service", "blocking", "reporting", "celebrity"
+    ]
+    df = df[df['title'].str.contains('|'.join(social_terms), case=False, na=False) |
+            df['text'].str.contains('|'.join(social_terms), case=False, na=False)]
     # Ensuring there aren't any missing / improper values for the rows
-    df = df.dropna(subset=['title', 'text', 'is_true'])
-    df = df[(df['title'] != "") & (df['text'] != "") & (df['is_true'] != "")]
-    # TODO: Sort by Social News
+    df = stage_df(df, df.columns.tolist())
     return df
